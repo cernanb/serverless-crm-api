8c96ae9e8d6a318e1f10e531ae7ea5ee9176cb75
cernanb <cernanb.@gmailcom>
2019-04-02 17:59:11 -0700

    Add get client function

---
+++ GetClient/index.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+const Client = require("../models/Client")
+const { cosmosDBAuth } = require("../utils")
+
+module.exports = function(context, req) {
+  context.log("JavaScript HTTP trigger function processed a request.")
+
+  mongoose.connect(
+    process.env.CosmosDBURL,
+    { auth: cosmosDBAuth },
+    (err, db) => {
+      if (err) throw err
+      Client.findById(context.req.params.id).exec(function(err, client) {
+        context.res = {
+          body: client
+        }
+
+        db.close()
+        context.done()
+      })
+    }
+  )
+}
